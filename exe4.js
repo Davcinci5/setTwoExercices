e75764a346e1b9555e4c96aa30a76c302e9d051a
Diego <diegoa.velascoc@gmail.com>
2020-08-27 16:24:24 -0700

    exercice 4 1

--- exe4.js
+++ exe4.js
@@ -1,7 +1,13 @@
 function querySelectorAll(input){//"div.note < input.is-complete[checked]"
     let elements = input.split(" ");
     if(elements.length > 3) return [];
-    return document.querySelectorAll(`${elements[0]} > ${elements[2]}`);
+    let list = [];
+    let parents = document.querySelectorAll(`${elements[0]}`);
+    for (let i = 0; i < parents.length; i++) {
+        const element = parents[i];
+        if(element.querySelector(`${elements[2]}`) !== null) list.push(element);
+    }
+    return list;
 }
 
 module.exports = querySelectorAll;
--- exe4.test.js
+++ exe4.test.js
@@ -1,6 +1,6 @@
 
 const querySelectorAll =  require('./exe4');
-test('return a list of three elements', () => {
+test('return a list of three elements using classes', () => {
     document.body.innerHTML = `
     <section>
         <div id="1" class="note">1<input type="checkbox" class="is-complete" checked> </div>
@@ -15,7 +15,7 @@ test('return a list of three elements', () => {
 
 });
 
-test('return a list of five elements', () => {
+test('return a list of five elements using classes', () => {
     document.body.innerHTML = `
     <section>
         <div id="1" class="note">1<input type="checkbox" class="is-complete" checked> </div>
@@ -30,20 +30,100 @@ test('return a list of five elements', () => {
 
 });
 
-test('return a list of three p elements', () => {
+test('return a div element  using classes', () => {
     document.body.innerHTML = `
     <div class="div1">
-        <p>Some div1 text</p>
-        <p>Some div1 text</p>
-        <p>Some div1 text</p>
+        <p class="note">Some div1 text</p>
+        <p class="note">Some div1 text</p>
+        <p class="note">Some div1 text</p>
 
         <div id="div2">
-        <p>Some div2 text</p>
-        <p>Some div2 text</p>
+            <p class="note">Some div2 text</p>
+            <p>Some div2 text</p>
+        </div class="note">
+    </div>
+    `;
+    let data = querySelectorAll("div.div1 < p.note");
+    expect(data.length).toBe(1);
+
+});
+test('return two divs elements using classes', () => {
+    document.body.innerHTML = `
+    <div class="div1">
+        <p class="note">Some div1 text</p>
+        <p class="note">Some div1 text</p>
+        <p class="note">Some div1 text</p>
+
+        <div class="div1">
+            <p class="note">Some div2 text</p>
+            <p>Some div2 text</p>
+        </div class="note">
+    </div>
+    `;
+    let data = querySelectorAll("div.div1 < p.note");
+    expect(data.length).toBe(2);
+
+});
+
+test('return one div element using id', () => {
+    document.body.innerHTML = `
+    <div class="div1">
+        <p class="note">Some div1 text</p>
+        <p class="note">Some div1 text</p>
+        <p class="note">Some div1 text</p>
+
+        <div id="div1">
+            <p class="note">Some div2 text</p>
+            <p>Some div2 text</p>
+        </div class="note">
+    </div>
+    `;
+    let data = querySelectorAll("div#div1 < p.note");
+    expect(data.length).toBe(1);
+
+});
+
+test('return a list of one element using pseudo-classes', () => {
+    document.body.innerHTML = `
+    <section>
+        <div id="1" class="note">1<input type="checkbox" class="is-complete"> </div>
+        <div id="3" class="note">3<input type="checkbox" class="is-complete" checked></div>
+        <div id="5" class="note">5<input type="checkbox" class="is-complete" checked></div>
+     </section>   
+    `;
+    let data = querySelectorAll("div.note < input.is-complete[type='checkbox']:not(:checked)");
+    expect(data.length).toBe(1);
+
+});
+
+test('return two divs element using id even one div is nested inside other', () => {
+    document.body.innerHTML = `
+    <div id="div1">
+        <p class="note">Some div1 text</p>
+        <p class="note">Some div1 text</p>
+        <p class="note">Some div1 text</p>
+
+        <div id="div1">
+            <p class="note">Some div2 text</p>
+            <p>Some div2 text</p>
+        </div class="note">
+    </div>
+    `;
+    let data = querySelectorAll("div#div1 < p.note");
+    expect(data.length).toBe(2);
+
+});
+
+test('return 1 div inside two divs ', () => {
+    document.body.innerHTML = `
+    <div class="outer">
+        <div class="select">
+            <div class="inner">
+            </div>
         </div>
     </div>
     `;
-    let data = querySelectorAll("div.div1 < p");
-    expect(data.length).toBe(3);
+    let data = querySelectorAll("div.select < div.inner");
+    expect(data.length).toBe(1);
 
 });
