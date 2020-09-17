5234988de90ac7250a82a32db8ca8105111cb7d6
Diego <diegoa.velascoc@gmail.com>
2020-09-17 09:34:53 -0700

    exercice 12.3

--- exe12.js
+++ exe12.js
@@ -1,54 +1,37 @@
 (function(global){
     let root = document.getElementById('results');
-
     const result = (text, pass) => {
         const el = document.createElement('li');
         el.textContent = text;
         pass!== undefined && (el.style.color = pass? 'green' : 'red');
         return el;
     };
-    let assert = (pass, message) => { return root.appendChild(result(message,pass));};
-    const test = (description, testBlock) =>{
+    let assert = (pass, message) => root.appendChild(result(message,pass));
+   
+    function test(description, testBlock){
         const parent = root;
         root = assert(undefined, description)
-            .appendChild(document.createElement('ul')); 
-        const wrapperLocalVariables = (root,cb) => {
-            let assert = (pass, message) => { return root.appendChild(result(message,pass));};
-            cb = cb.toString();
-            cb = cb.slice(cb.indexOf("{") + 1, cb.lastIndexOf("}"));
-            return () =>{
-                eval(cb);
-            };
-        };
-        let wrapper = wrapperLocalVariables(root,testBlock);
-        wrapper();
-        root = parent;
-    };
+            .appendChild(document.createElement('ul'));
+
+        (function(_root){
+            let originalSet =  global.setTimeout,
+                assertParent = global.assert;
+
+            function _setTimeout(newCb,time,...args){                
+                let customizedCB = () =>{
+                    global.assert = (pass, message) => { return _root.appendChild(result(message,pass));};
+                    newCb(...args);
+                };
+                originalSet(customizedCB,time);
+            }
+            global.assert = (pass, message) => { return _root.appendChild(result(message,pass));};   
+            global.setTimeout = _setTimeout;
+                            
+            testBlock();
+            global.assert = assertParent;
+        })(root);
+        root=parent;
+    }
     global.assert = assert;
     global.test = test;
 })(window);
-
-
-
-// let assert1 = (bool,text) =>{
-//     console.log(bool);
-// };
-
-// function one() {
-//     assert1(true, "Inside TestBlock B");
-//     setTimeout(assert1, 500, true, "test delayed B");
-// };
-
-// function two(string_cb){
-//     let assert1 = (bool,text) =>{
-//         console.log(text);
-//     };
-//     return ()=>{
-//         var body = string_cb.slice(string_cb.indexOf("{") + 1, string_cb.lastIndexOf("}"));
-//         console.log(body);
-//         eval(body);
-//     };
-// }
-
-// let three = two(one.toString());
-// three();
